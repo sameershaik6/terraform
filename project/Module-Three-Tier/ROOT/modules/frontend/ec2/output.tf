0549c36122095f99c84946a4fd6e96b0986545e6
sameershaik6 <sameerthsk@gmail.com>
2026-07-27 12:50:49 +0530

    project-terraform

---
+++ project/Module-Three-Tier/ROOT/modules/frontend/ec2/output.tf
@@ -0,0 +1,10 @@
+output "frontend_private_ip" {
+  value = aws_instance.frontend.private_ip
+}
+
+output "frontend_instance_id" {
+  value = aws_instance.frontend.id
+}
+output "frontend_instanceid" {
+  value = aws_instance.frontend.id
+}
